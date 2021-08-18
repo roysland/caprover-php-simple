05a36800a1b0ab1d0a43b7221b527069096d7090
Øyvind Røysland <oyvind.roysland@schibsted.com>
2021-08-18 21:28:44 +0200

    add composer

--- index.php
+++ index.php
@@ -1,18 +1,43 @@
 <?php
+require 'vendor/autoload.php';
+use Parse\ParseClient;
+use Parse\ParseObject;
+use Parse\ParseException;
+use Parse\ParseQuery;
+$app_id = 'parseapp';
+$rest_key = null;
+$master_key = 'P0s1M0rt3m';
 
-$action = 'default';
+ParseClient::initialize($app_id, $rest_key, $master_key);
+ParseClient::setServerURL('https://parse-parse.rover.pageup.no', 'parse');
 
-if (isset($_GET['action'])) {
-  $action = $_GET['action'];
+$health = ParseClient::getServerHealth();
+if ($health['status'] === 200) {
+  print('Everything looks good');
+  // addGameObject();
+  getObject();
 }
 
-switch ($action) {
-  case 'default':
-    echo 'It works. Now test something else';
-    break;
-  case 'random':
-      $query = file_get_contents('https://randomuser.me/api/');
-      $response = json_decode($query);
-      print_r($response);
-    break;
+function addGameObject () {
+  $score = new ParseObject('GameScore');
+  $score->set('score', 1337);
+  $score->set('name', 'Øyvind');
+  $score->set('active', true);
+  try {
+    $score->save();
+    echo 'New object created with objectId: ' . $score->getObjectId();
+  } catch (ParseException $ex) {
+    echo "Failed to create new object: ". $ex->getMessage();
+  }
+}
+
+function getObject () {
+  $query = new ParseQuery('GameScore');
+  try {
+    $score = $query->get('uETw8nban9');
+    print_r($score);
+  }
+  catch (ParseException $ex) {
+    echo "Failed to create new object: ". $ex->getMessage();
+  }
 }
